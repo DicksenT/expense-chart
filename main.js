4d088b411348b696d4bf6a7d9523dcf9612e2ce8
Dicksen Tan <tandicksen@gmail.com>
2024-02-01 20:43:44 +0700

    all done except hover effect

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-const bars = document.querySelectorAll('.bar')
-const dayNum = document.querySelectorAll('.day-num')
+const bars = document.querySelectorAll(".bar");
+const dayNum = document.querySelectorAll(".day-num");
+
+async function fetchData() {
+  try {
+    const response = await fetch("data.json");
+    const data = await response.json();
+    const highest = data.reduce((max, cur) => {
+      return cur.amount > max.amount ? cur : max;
+    }, data[0]);
+    for (let i = 0; i < bars.length; i++) {
+      bars[i].style.height = (data[i].amount / highest.amount) * 140 + "px";
+      if (data[i].amount === highest.amount) {
+        bars[i].style.backgroundColor = "hsl(186, 34%, 60%)";
+      }
+      dayNum[i].innerText = `$${data[i].amount}`;
+      bars[i].addEventListener('mouseenter', function(){
+        dayNum[i].style.opacity = '1'
+      })
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+fetchData();
